e19b15e06049bfe48389ed7e0b59b5c5dfd62532
dayAlone <andrey.slider@gmail.com>
2015-02-24 11:27:01 +0500

    Map

--- public_html/local/templates/.default/components/bitrix/news.list/map/template.php
+++ public_html/local/templates/.default/components/bitrix/news.list/map/template.php
@@ -5,16 +5,7 @@
         <div class="geography__popup_toolbar">
         	<a href="#" class="geography__popup_close"><?=svg('close')?> Закрыть</a></div>
         <div class="geography__popup_content">
-          <?/*<div class="scroll">
-            <h4><span>Заказчик</span></h4>
-            <h2>Роснефть</h2>
-            <h4><span>Объект</span></h4>
-            <p>РН-Туапсинский НПЗ</p>
-            <h4><span>Проект</span></h4>
-            <p>Комбинированная установка КУ-3. Объект 672-10. Секция «Производство элементарной серы», «Доочистка хвостовых газов», «Узел дегазации серы»</p>
-            <p>Комбинированная установка КУ-3. Объект 672-10. Секция «Производство элементарной серы», «Доочистка хвостовых газов», «Узел дегазации серы»</p><img src="/layout/images/img.jpg">
-          </div>
-          */?>
+          
         </div>
       </div>
       <div id="map"></div>
@@ -22,9 +13,9 @@
       	$data = array();
       	foreach ($arResult['ITEMS'] as $key=>$item):
       		$data[] = array('id'=>$item['ID'], 'name'=>$item['NAME'], 'coords'=>$item['PROPERTIES']['COORDS']['VALUE'], 'url'=>$item['DETAIL_PAGE_URL']);
-		endforeach;
-    $json = json_encode($data, JSON_UNESCAPED_UNICODE);
-    var_dump($json);
+    		endforeach;
+        $json = json_encode($data, JSON_UNESCAPED_UNICODE);
+        var_dump($json);
 	  ?>
 
       <script>mapItems = <?=(strlen($json)>0?$json:"")?></script>
--- public_html/works/maps/index.php
+++ public_html/works/maps/index.php
@@ -1,11 +1,14 @@
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle('Карта проектов');
+global $arMaps;
+$arMaps = array("!PROPERTY_COORDS"=>false);
 $APPLICATION->IncludeComponent("bitrix:news.list", "map", 
 	array(
 	  "IBLOCK_ID"      => 2,
-	  "NEWS_COUNT"     => "6123123123123",
+	  "NEWS_COUNT"     => "100000",
 	  "SORT_BY1"       => "SORT",
+	  "FILTER_NAME"    => "arMaps",
 	  "SORT_ORDER1"    => "ASC",
 	  "DETAIL_URL"     => "/works/projects/#ELEMENT_CODE#/",
 	  "CACHE_TYPE"     => "A",
