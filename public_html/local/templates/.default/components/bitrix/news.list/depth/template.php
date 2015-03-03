61fe221d38130fd8a20554a9ada7f8c32ee4b555
dayAlone <andrey.slider@gmail.com>
2015-03-04 00:33:45 +0500

    Changes

--- public_html/local/templates/.default/components/bitrix/news.list/depth/template.php
+++ public_html/local/templates/.default/components/bitrix/news.list/depth/template.php
@@ -2,7 +2,7 @@
 <?if(count($arResult['ITEMS'])>0):?>
 <div class="row xxl-margin-top row--center center s-padding-top m-padding-bottom no-gutter row--center">
 <?foreach ($arResult['ITEMS'] as $key=>$item):
-	?><div class="col-xs-12 col-sm-4 col-md-2 <?=($key==0?"col-md-offset-1":"")?>"><a href="<?=$item['DETAIL_PAGE_URL']?>" class="depth"><?=$item['ICON']?><br><?=$item['NAME']?></a></div><?
+	?><div class="col-xs-12 col-sm-4 col-md-2"><a href="<?=$item['DETAIL_PAGE_URL']?>" class="depth"><?=$item['ICON']?><br><?=$item['NAME']?></a></div><?
 endforeach;?>
 <?=($arParams["DISPLAY_BOTTOM_PAGER"]=="Y" ? $arResult["NAV_STRING"]:"")?>
 </div>
