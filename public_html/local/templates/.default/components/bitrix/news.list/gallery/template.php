7ef5940d23d55ec1c3bf4a118cd2abc816d63e69
dayAlone <andrey.slider@gmail.com>
2015-03-05 12:04:15 +0500

    Photos

--- public_html/local/templates/.default/components/bitrix/news.list/gallery/template.php
+++ public_html/local/templates/.default/components/bitrix/news.list/gallery/template.php
@@ -3,13 +3,10 @@
 <div class="projects gallery">
 <?foreach ($arResult['ITEMS'] as $key=>$item):
 	$img = $item['PREVIEW_PICTURE'];
-	?><figure itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">
-    	<a href="<?=$img['SRC']?>" class="project" itemprop="contentUrl" data-size="<?=$img['WIDTH']."x".$img['HEIGHT']?>" style="background-image:url(<?=$img['SMALL']?>)">
+	?><figure itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject"><a href="<?=$img['SRC']?>" class="project" itemprop="contentUrl" data-size="<?=$img['WIDTH']."x".$img['HEIGHT']?>" style="background-image:url(<?=$img['SMALL']?>)">
     		<img src="<?=$img['SRC']?>">
     		<span><?=$item['NAME']?></span>
-    	</a>
-    	<figcaption itemprop="caption description hidden"><?=$item['NAME']?></figcaption>
-    </figure><?
+    	</a><figcaption itemprop="caption description hidden"><?=$item['NAME']?></figcaption></figure><?
     endforeach;?>
 </div>
 <?endif;?>
