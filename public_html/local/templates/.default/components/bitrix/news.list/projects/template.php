896afd9b083861d2ac483a72d2bad9a8fbfe7398
dayAlone <andrey.slider@gmail.com>
2015-03-10 11:16:26 +0500

    Changes

--- public_html/local/templates/.default/components/bitrix/news.list/projects/template.php
+++ public_html/local/templates/.default/components/bitrix/news.list/projects/template.php
@@ -3,7 +3,7 @@ if(count($arResult['ITEMS'])>0):?>
 <div class="projects">
 <?foreach ($arResult['ITEMS'] as $key=>$item):
 	?><a href="<?=$item['DETAIL_PAGE_URL']?>" style="background-image: url(<?=$item['PREVIEW_PICTURE']['SRC']?>)" class="project">
-		<span><?=$item['NAME']?></span>
+		<span><?=html_entity_decode($item['NAME'])?></span>
 	</a><?
 endforeach;?>
 <?=($arParams["DISPLAY_BOTTOM_PAGER"]=="Y" ? $arResult["NAV_STRING"]:"")?>
