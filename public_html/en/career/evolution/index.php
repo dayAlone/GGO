370f3d29d486b8a29a8acac645d74561ef97c1d6
dayAlone <andrey.slider@gmail.com>
2015-04-27 19:14:56 +0500

    Development

--- public_html/en/career/evolution/index.php
+++ public_html/en/career/evolution/index.php
@@ -2,33 +2,24 @@
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle("Professional development");
 ?><p class="highlight">
-	В 2010 году <strong>создан Корпоративный университет инжиниринга</strong> - механизм системного формирования профессиональных компетенций и личностных качеств сотрудников в соответствии с корпоративной культурой и стратегией развития группы компаний.
+	In 2010, <strong>the Company launched Corporate University of Engineering</strong> that serves as a tool for systematic development of employees’ professional competencies and personal qualities in keeping with the corporate culture and the development strategy pursued by the Group.
 </p>
 <div class="page__divider page__divider--small">
 </div>
-<h4>Разноформатное обучение:</h4>
+<h4>The study program uses a variety of formats:</h4>
 <p>
-	Лекции, вебинары, мастер-классы, мозговые штурмы, тренинги, выездные семинары, дистанционные курсы и др. проходят без отрыва от работы.
+	Lectures, webinars, master classes, brain-storming sessions, workshops, retreat sessions, distance learning courses and other learning formats that allow participants to study on the job.   
 </p>
-<h4>Обучение проводят:</h4>
+<h4>The courses are taught by:</h4>
 <p>
-	МГУ им. Ломоносова, ГУУ, НИУ ВШЭ, РАНХиГС, РГУ нефти и газа им. И.М. Губкина, РХТУ имени Д.И. Менделеева, ведущие специалисты отечественных и зарубежных компаний, ТОП менеджмент группы компаний «Гипрогазоочистка».
+	Academic staff from Lomonosov Moscow State University, State University of Management, Higher School of Economics, the Russian Presidential Academy of National Economy and Public Administration, Gubkin Russian State Oil and Gas University, D. Mendeleev University of Chemical Technology of Russia, as well as leading professionals from Russian and foreign companies and top managers from Giprogazoochistka Group.
 </p>
-<h4>Постоянно действующие программы:</h4>
-<p>
-	«Кадровый резерв», «Техническое обучение», «Промышленная безопасность», «МСФО», «Непрерывный английский», «Проектная деятельность», «ТРИЗ», «Отраслевой Форсайт».
-</p>
-<p>
-	В 2012 – 2014 гг. - совместно специалистами ГГО и учеными РГУ нефти и газа имени И. М. Губкина <strong>разработана программа и проведено обучение по темам:</strong>&nbsp;"Подготовка и переработка нефти и газа", "Технологии и оборудование",&nbsp; "Основы проектирования схем электроснабжения объектов нефтегазового комплекса".
-</p>
-<h4>Образовательные ресурсы:</h4>
-<p>
-	Библиотека профессиональной и управленческой литературы, профессиональные площадки, специализированные рассылки, образовательный портал, система дистанционного обучения.
-</p>
-<hr>
-<p>
-	С дополнительной информацией Вас ознакомят в Дирекции Корпоративного университета, по телефону +7 (495) 231-30-40 доб. 4470
-</p>
+<h4>Continuous study programs:</h4>
+<p>Talent Pool Program, Technical Training, Industrial Safety, International Financial Reporting Standards, Continuing English Language Program, Project Work, the Theory of Inventive Problem Solving, and Industry Foresight.</p>
+<p>In 2012 – 2014, in a joint effort with scientists from Gubkin Russian State Oil and Gas University, GGO specialists developed study curriculum and taught courses on topics including Oil and Gas Treatment and Refining, Processes and Equipment, and Engineering Basics for Power Supply Systems at Oil and Gas Facilities.  </p>
+<h4>Learning resources:</h4>
+<p>A library of specialist and management books, specialized bulletins, a knowledge repository portal, and a distance learning system. </p>
+<p>For additional information, please contact Corporate University administration at +7 (495) 231-30-40, ext. 4470     </p>
  <br><?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
