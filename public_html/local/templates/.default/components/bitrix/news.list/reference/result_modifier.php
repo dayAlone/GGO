a037fd4a139a5632b40aa967298980b26e9795b9
dayAlone <andrey.slider@gmail.com>
2015-05-20 17:13:20 +0500

    Changes

--- public_html/local/templates/.default/components/bitrix/news.list/reference/result_modifier.php
+++ public_html/local/templates/.default/components/bitrix/news.list/reference/result_modifier.php
@@ -1,12 +1,24 @@
 <?
 	$arResult['SECTIONS'] = array();
+	function reference_sort($a, $b)
+    {
+        if($a['sort']=='' && $b['sort']>0) 
+            return 1;
+        if($b['sort']=='' && $a['sort']>0) 
+            return -1;
+        if($a['sort']=='' && $b['sort']=='')
+            return ($a['value'] < $b['value']) ? -1 : 1;
+        if ($a['sort'] == $b['sort'])
+            return 0;
+        return ($a['sort'] < $b['sort']) ? -1 : 1;
+    }	
 	foreach ($arResult['ITEMS'] as $key=>$item):
 		if(!isset($arResult['SECTIONS'][$item['IBLOCK_SECTION_ID']]))
 			$arResult['SECTIONS'][$item['IBLOCK_SECTION_ID']] = array('ELEMENTS' => array());
 		$arResult['SECTIONS'][$item['IBLOCK_SECTION_ID']]['ELEMENTS'][] = array(
 			'NAME'   => $item['NAME'],
 			'CODE'   => $item['CODE'],
-			'SORT'   => $item['SORT'],
+			'sort'   => $item['SORT'],
 			'client' => $item['PROPERTIES']['CLIENT']['VALUE'],
 			'object' => $item['PROPERTIES']['OBJECT']['VALUE']['TEXT'],
 			'region' => $item['PROPERTIES']['REGION']['VALUE'],
@@ -17,6 +29,10 @@
 			);
 	endforeach;
 	
+	foreach ($arResult['SECTIONS'] as $key=>$item):
+		usort($item['ELEMENTS'], "reference_sort");
+	endforeach;
+
 	$arFilter = array('IBLOCK_ID' => $arResult['ID'], 'ID'=>array_keys($arResult['SECTIONS']));
    	$rsSect = CIBlockSection::GetList(array('left_margin' => 'asc'),$arFilter);
    	while ($arSect = $rsSect->Fetch()) {
