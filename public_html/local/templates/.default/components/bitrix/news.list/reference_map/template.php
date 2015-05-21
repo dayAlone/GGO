bdc1185fae53d752ebf672295f95a4a951555014
dayAlone <andrey.slider@gmail.com>
2015-05-21 10:32:59 +0500

    Map

--- public_html/local/templates/.default/components/bitrix/news.list/reference_map/template.php
+++ public_html/local/templates/.default/components/bitrix/news.list/reference_map/template.php
@@ -4,7 +4,6 @@
 	foreach ($arResult['ITEMS'] as $item):
 		if(strlen($item['PROPERTIES']['COORDS']['VALUE']) > 0)
 			$data[] = array('id'=>$item['ID'].$key, 'name'=>$item['PROPERTIES']['OBJECT']['VALUE']['TEXT'], 'coords'=>$item['PROPERTIES']['COORDS']['VALUE'], 'url'=>$item['DETAIL_PAGE_URL'].$key."/");
-		endforeach;
 	endforeach;
 	$json = json_encode($data, JSON_UNESCAPED_UNICODE);
 ?>
