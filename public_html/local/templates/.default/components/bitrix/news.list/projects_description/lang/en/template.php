44b2762d74212b726a411bb56fb40861c4d1a203
dayAlone <andrey.slider@gmail.com>
2015-05-23 00:26:27 +0500

    Changes

--- public_html/local/templates/.default/components/bitrix/news.list/projects_description/lang/en/template.php
+++ public_html/local/templates/.default/components/bitrix/news.list/projects_description/lang/en/template.php
@@ -5,6 +5,7 @@ $MESS["SPEED"] = "Capcity";
 $MESS["TOOLS"] = "Processes perfomance";
 $MESS['DETAILS'] = 'Distinctive features of the Project';
 
+$MESS['PCT'] = 'Project';
 $MESS['T'] = 'Technologies';
 $MESS['P'] = 'Capcity';
 $MESS['N'] = 'Process performance ';
