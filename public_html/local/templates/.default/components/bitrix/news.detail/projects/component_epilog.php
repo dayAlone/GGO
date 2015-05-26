167abe14cf9be5df8dbe726d96f53291bececc60
dayAlone <andrey.slider@gmail.com>
2015-05-26 16:07:20 +0500

    Changes

--- public_html/local/templates/.default/components/bitrix/news.detail/projects/component_epilog.php
+++ public_html/local/templates/.default/components/bitrix/news.detail/projects/component_epilog.php
@@ -1,8 +1,8 @@
 <?
 if(intval($arResult['IBLOCK_SECTION_ID'])>0):
+	CModule::IncludeModule("iblock");
 	$res = CIBlockSection::GetByID($arResult['IBLOCK_SECTION_ID']);
 	if($ar_res = $res->GetNext()):
-		var_dump();
 		LocalRedirect($ar_res['LIST_PAGE_URL'].$ar_res['CODE']."/");
 	endif;	
 endif;
