7317eedab13f5b210de9ad9aaaca72eb6be29bd8
dayAlone <andrey.slider@gmail.com>
2015-08-25 13:13:15 +0500

    Map

--- public_html/local/templates/.default/components/bitrix/news.detail/map/template.php
+++ public_html/local/templates/.default/components/bitrix/news.detail/map/template.php
@@ -10,7 +10,7 @@ $item = $arResult;
   <? 
   endif;
 endforeach;?>
-<h4><span>Объект</span></h4>
+<h4><span><?=LANGUAGE_ID=="ru"?"Объект":"Object")?></span></h4>
 <p><?=html_entity_decode($item['NAME'])?></p>
 <? foreach(array('WORKTYPE', 'TIME') as $prop):
   if(strlen($item['PROPERTIES'][$prop]['VALUE']['TEXT'])>0):
@@ -21,7 +21,7 @@ endforeach;?>
   endif;
 endforeach;?>
 <?if($item['PROPERTIES']['TABLE']['VALUE']):?>
-<h4><span>Технологии</span></h4>
+<h4><span><?=LANGUAGE_ID=="ru"?"Технологии":"Technologies")?></span></h4>
 <? foreach($item['PROPERTIES']['TABLE']['VALUE'] as $row):?>
   <?if(strlen($row['t0'])>0):?>
     <p><?=$row['t0']?></p>
@@ -29,7 +29,7 @@ endforeach;?>
 <? endforeach;?>
 <?endif;?>
 
-<h4><span>Особенности проекта</span></h4>
+<h4><span><?=LANGUAGE_ID=="ru"?"Особенности проекта":"Description")?></span></h4>
 <?=$item["~DETAIL_TEXT"]?>
 <?if(strlen($item['PREVIEW_PICTURE']['SRC'])>0):?>
 <img src="<?=$item['PREVIEW_PICTURE']['SRC']?>" class="max-width">
